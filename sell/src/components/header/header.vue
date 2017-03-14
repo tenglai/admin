6d354cc3082bc0e5e751ea58e605d69afe976988
tenglai <13049322087@163.com>
2017-03-15 00:22:45 +0800

    浮层

--- sell/src/components/header/header.vue
+++ sell/src/components/header/header.vue
@@ -17,24 +17,62 @@
   				<span class="text">{{seller.supports[0].description}}</span>
   			</div>
   		</div>
-  		<div v-if="seller.supports" class="support-count">
-  			<span class="count">{{seller.supports.length}}</span>
+  		<div v-if="seller.supports" class="support-count" @click="showDetail">
+  			<span class="count">{{seller.supports.length}}个</span>
   			<i class="icon-keyboard_arrow_right"></i>
   		</div>
   	</div>
-  	<div class="bulletin-wrapper"></div>
+  	<div class="bulletin-wrapper" @click="showDetail">
+  		<span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
+  		<i class="icon-keyboard_arrow_right"></i>
+  	</div>
+  	<div class="background">
+  		<img :src="seller.avatar" width="100%" height="100%" />
+  	</div>
+  	<div v-show="detailShow" class="detail">
+  		<div class="detail-wrapper clearfix">
+  			<div class="detail-main">
+  				<h1 class="name">{{seller.name}}</h1>
+  				<div class="star-wrapper">
+  					<star :size="48" :score="seller.score"></star>
+  				</div>
+  				<div class="title">
+  					<div class="line"></div>
+  					<div class="text">优惠信息</div>
+  					<div class="line"></div>
+  				</div>
+  			</div>
+  		</div>
+  		<div class="detail-close">
+  			<i class="icon-close"></i>
+  		</div>
+  	</div>
   </div>
 </template>
 
-<script>
+<script type="text/ecmascript-6">
+	import star from 'components/star/star'
 export default {
 	props: {
 		seller: {
 			type: Object
 		}
 	},
+	data() {
+		return {
+			detailShow: false
+		}
+	},
+	methods: {
+		showDetail() {
+			this.detailShow = true
+		}
+	},
 	created() {
 		this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee']
+	},
+	components: {
+		star
 	}
 }
 </script>
@@ -43,9 +81,12 @@ export default {
 	@import "../../common/scss/mixin.scss";
 	
 	.header{
+		position:relative;
 		color: #fff;
-		background: #000;
+		overflow:hidden;
+		background:rgba(7,17,27,0.5);
 		.content-wrapper{
+			position: relative;
 			padding: 24px 12px 18px 24px;
 			font-size:0;
 			.avatar{
@@ -111,6 +152,118 @@ export default {
 						font-size: 10px;
 					}
 				}
+			}
+			.support-count{
+				position: absolute;
+				right: 12px;
+				bottom: 14px;
+				padding: 0px 8px;
+				height: 24px;
+				line-height: 24px;
+				border-radius: 14px;
+				background: rgba(0,0,0,0.2);
+				text-align: center;
+				.count{
+					font-size: 10px;
+					vertical-align: top;
+				}
+				.icon-keyboard_arrow_right{
+					margin-left: 2px;
+					line-height: 24px;
+					font-size: 10px;
+				}
+			}
+		}
+		.bulletin-wrapper{
+			position: relative;
+			height: 28px;
+			line-height: 28px;
+			padding: 0px 22px 0px 12px;
+			white-space: nowrap;
+			overflow: hidden;
+			text-overflow: ellipsis;
+			background: rgba(7,17,27,0.2);
+			.bulletin-title{
+				display: inline-block;
+				vertical-align: top;
+				margin-top: 8px;
+				width: 22px;
+				height: 12px;
+				@include bg-image('bulletin');
+				background-size: 22px 12px;
+				background-repeat: no-repeat;
+			}
+			.bulletin-text{
+				vertical-align: top;
+				margin: 0px 4px;
+				font-size: 10px;
+			}
+			.icon-keyboard_arrow_right{
+				position: absolute;
+				font-size: 10px;
+				right: 12px;
+				top: 8px;
+			}
+		}
+		.background{
+			position: absolute;
+			top: 0;
+			left: 0;
+			width: 100%;
+			height: 100%;
+			z-index: -1;
+			filter: blur(10px);
+		}
+		.detail{
+			position: fixed;
+			top: 0;
+			left: 0;
+			z-index: 100;
+			width: 100%;
+			height: 100%;
+			overflow: auto;
+			background: rgba(7,17,27,0.8);
+			.detail-wrapper{
+				width: 100%;
+				min-height: 100%;
+				.detail-main{
+					margin-top: 64px;
+					padding-bottom: 64px;
+					.name{
+						line-height: 16px;
+						text-align: center;
+						font-size: 16px;
+						font-weight: 700;
+					}
+					.star-wrapper{
+						margin-top: 18px;
+						padding: 2px 0px;
+						text-align: center;
+					}
+					.title{
+						display: flex;
+						width: 80%;
+						margin: 30px auto 24px auto;
+						.line{
+							flex: 1;
+							position: relative;
+							top: -6px;
+							border-bottom: 1px solid rgba(255,255,255,1);
+						}
+						.text{
+							padding: 0 12px;
+							font-size: 14px;
+						}
+					}
+				}
+			}
+			.detail-close{
+				position: relative;
+				width: 32px;
+				height: 32px;
+				margin: -64px auto 0 auto;
+				clear: both;
+				font-size: 32px;
 			}
 		}
 	}
