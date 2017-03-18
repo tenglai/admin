a6ac83993be3762d41ce96c1ac67f70f31edd39d
tenglai <13049322087@163.com>
2017-03-18 23:25:56 +0800

    better-scroll

--- sell/src/components/goods/goods.vue
+++ sell/src/components/goods/goods.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="goods">
-		<div class="menu-wrapper">
+		<div class="menu-wrapper" v-el:menu-wrapper>
 			<ul>
 				<li v-for="item in goods" class="menu-item">
 					<span class="text border-1px">
@@ -9,25 +9,23 @@
 				</li>
 			</ul>
 		</div>
-		<div class="foods-wrapper">
+		<div class="foods-wrapper" v-el:foods-wrapper>
 			<ul>
 				<li v-for="item in goods" class="food-list">
 					<h1 class="title">{{item.name}}</h1>
 					<ul>
-						<li v-for="food in item.foods" class="food-item">
+						<li v-for="food in item.foods" class="food-item border-1px">
 							<div class="icon">
-								<img :src="food.icon" />
+								<img width="57" height="57" :src="food.icon" />
 							</div>
 							<div class="content">
 								<h2 class="name">{{food.name}}</h2>
 								<p class="desc">{{food.description}}</p>
 								<div class="extra">
-									<span>月售{{food.sellCount}}份</span>
-									<span>好评率{{food.rating}}%</span>
+									<span class="count">月售{{food.sellCount}}份</span><span>好评率{{food.rating}}%</span>
 								</div>
 								<div class="price">
-									<span>￥{{food.price}}</span>
-									<span v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+									<span class="now">￥{{food.price}}</span><span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
 								</div>
 							</div>
 						</li>
@@ -39,6 +37,7 @@
 </template>
 
 <script type="text/ecmascript-6">
+import BScroll from 'better-scroll'
 const ERR_OK = 0
 export default {
 	props: {
@@ -57,8 +56,17 @@ export default {
 			response = response.body
 			if (response.errno === ERR_OK) {
 				this.goods = response.data
+				this.$nextTick(() => {
+					this._initScroll()
+				})
 			}
 		})
+	},
+	methods: {
+		_initScroll() {
+			this.menuScroll = new BScroll(this.$els.menuWrapper, {})
+			this.foodsScroll = new BScroll(this.$els.foodsWrapper, {})
+		}
 	}
 }
 </script>
@@ -118,6 +126,67 @@ export default {
 		}
 		.foods-wrapper{
 			flex: 1;
+			.title{
+				padding-left: 14px;
+				height: 26px;
+				line-height: 26px;
+				border-left: 2px solid #d9ddea1;
+				font-size: 12px;
+				color: rgb(147,153,159);
+				background:#f3f5f7;
+			}
+			.food-item{
+				display: flex;
+				margin: 18px;
+				padding-bottom: 18px;
+				@include border-1px(rgba(7,17,27,0.1));
+				&:last-child{
+					@include border-none();
+					margin-bottom:0;
+				}
+			}
+			.icon{
+				flex: 0 0 57px;
+				margin-right: 10px;
+			}
+			.content{
+				flex:1;
+				.name{
+					margin: 2px 0 8px 0;
+					height: 14px;
+					line-height: 14px;
+					font-size: 14px;
+					color: rgb(7,17,27);
+				}
+				.desc,.extra{
+					line-height: 10px;
+					font-size: 10px;
+					color: rgb(147,153,159);
+				}
+				.desc{
+					line-height: 12px;
+					margin-bottom: 8px;
+				}
+				.extra{
+					.count{
+						margin-right: 12px;
+					}
+				}
+				.price{
+					font-weight: 700;
+					line-height: 24px;
+					.now{
+						margin-right: 8px;
+						font-size: 14px;
+						color: rgb(240,20,20);
+					}
+					.old{
+						text-decoration: line-through;
+						font-size: 10px;
+						color: rgb(147,153,159);
+					}
+				}
+			}
 		}
 	}
 </style>
