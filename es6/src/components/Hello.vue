6e9a4cb89a3cf5d9680fddaeb76b13bc22f696a4
tenglai <13049322087@163.com>
2017-04-03 16:52:32 +0800

    demo

--- es6/src/components/Hello.vue
+++ es6/src/components/Hello.vue
@@ -24,10 +24,47 @@
 export default {
   name: 'hello',
   data () {
+    studyEs6()
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: '欢迎 to Your Vue.js App'
     }
   }
+}
+function studyEs6() {
+  // let [a,b,c,d] = ['aa','bb',77,88]
+  // alert(c)
+  // let [a,b,[c,d],e] = ['aa','bb',[33,44],55]
+  // alert(d)
+  // let [a,b,,e] = ['aa','bb',[33,44],55]
+  // alert(e)
+  // let [a,b,,e,f] = ['aa','bb',[33,44],55]
+  // alert(f)  // undefined
+  // let [a,b,,e,f='hello'] = ['aa','bb',[33,44],55]
+  // alert(f)
+  // let [a,b,,e,f='hello'] = ['aa','bb',[33,44],55,'ff']
+  // alert(f)
+  let obj = new Object()
+  obj.uid = 111
+  obj.uname = '张三'
+  obj.arr = ['aa','bb']
+  // let {uid:id,uname:name} = obj // 顺序改变无影响
+  // alert(name)
+  // let uid,uname,arr
+  // ({uid,uname} = obj)
+  // alert(uname)
+  // let uid,uname,arr,a,b
+  // ({arr:[a,b]} = obj)
+  // alert(a)  // a,b需要定义
+  // let uid,uname,arr,a,b,c
+  // ({arr:[a,b,c='ccc']} = obj)
+  // alert(c)  // 默认值c
+  // let [a,b,c,d] = '倚天屠龙'
+  // console.log(a,b,c,d)
+  analysis(obj)
+}
+function analysis ({uid,uname}) {
+  alert(uid)
+  alert(uname)
 }
 </script>
 
