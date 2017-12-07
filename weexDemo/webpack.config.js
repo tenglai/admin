d3883067ba05f1df682b617d990814859870d862
tenglai <13049322087@163.com>
2017-12-07 20:03:22 +0800

    change webpack.config.js

--- weexDemo/webpack.config.js
+++ weexDemo/webpack.config.js
@@ -11,53 +11,53 @@ const isWin = /^win/.test(process.platform);
 let fileType = '';
 // Wraping the entry file
 const getEntryFileContent = (entryPath, vueFilePath) => {
-    let relativePath = pathTo.relative(pathTo.join(entryPath, '../'), vueFilePath);
-    let contents = '';
-    /**
-     * The plugin's logic currently only supports the .we version
-     * which will be supported later in .vue
-     */
-    if (hasPluginInstalled) {
-      const plugindir = pathTo.resolve('./web/plugin.js');
-      contents = 'require(\'' + plugindir + '\') \n';
-    }
-    if (isWin) {
-      relativePath = relativePath.replace(/\\/g, '\\\\');
-    }
-    contents += 'var App = require(\'' + relativePath + '\')\n';
-    contents += 'App.el = \'#root\'\n';
-    contents += 'new Vue(App)\n';
-    return contents;
+  let relativePath = pathTo.relative(pathTo.join(entryPath, '../'), vueFilePath);
+  let contents = '';
+  /**
+   * The plugin's logic currently only supports the .we version
+   * which will be supported later in .vue
+   */
+  if (hasPluginInstalled) {
+    const plugindir = pathTo.resolve('./web/plugin.js');
+    contents = 'require(\'' + plugindir + '\') \n';
   }
-  // Retrieve entry file mappings by function recursion
+  if (isWin) {
+    relativePath = relativePath.replace(/\\/g, '\\\\');
+  }
+  contents += 'var App = require(\'' + relativePath + '\')\n';
+  contents += 'App.el = \'#root\'\n';
+  contents += 'new Vue(App)\n';
+  return contents;
+}
+// Retrieve entry file mappings by function recursion
 const walk = (dir) => {
-    dir = dir || '.';
-    const directory = pathTo.join(__dirname, 'src', dir);
-    fs.readdirSync(directory).forEach((file) => {
-      const fullpath = pathTo.join(directory, file);
-      const stat = fs.statSync(fullpath);
-      const extname = pathTo.extname(fullpath);
-      if (stat.isFile() && extname === '.vue' || extname === '.we') {
-        if (!fileType) {
-          fileType = extname;
-        }
-        if (fileType && extname !== fileType) {
-          console.log('Error: This is not a good practice when you use ".we" and ".vue" togither!');
-        }
-        const name = pathTo.join(dir, pathTo.basename(file, extname));
-        if (extname === '.vue') {
-          const entryFile = pathTo.join(vueWebTemp, dir, pathTo.basename(file, extname) + '.js');
-          fs.outputFileSync(pathTo.join(entryFile), getEntryFileContent(entryFile, fullpath));
-          entry[name] = pathTo.join(__dirname, entryFile) + '?entry=true';
-        }
-        weexEntry[name] = fullpath + '?entry=true';
-      } else if (stat.isDirectory() && file !== 'build' && file !== 'include') {
-        const subdir = pathTo.join(dir, file);
-        walk(subdir);
+  dir = dir || '.';
+  const directory = pathTo.join(__dirname, 'src', dir);
+  fs.readdirSync(directory).forEach((file) => {
+    const fullpath = pathTo.join(directory, file);
+    const stat = fs.statSync(fullpath);
+    const extname = pathTo.extname(fullpath);
+    if (stat.isFile() && extname === '.vue' || extname === '.we') {
+      if (!fileType) {
+        fileType = extname;
       }
-    });
-  }
-  // Generate an entry file before writing a webpack configuration
+      if (fileType && extname !== fileType) {
+        console.log('Error: This is not a good practice when you use ".we" and ".vue" togither!');
+      }
+      const name = pathTo.join(dir, pathTo.basename(file, extname));
+      if (extname === '.vue') {
+        const entryFile = pathTo.join(vueWebTemp, dir, pathTo.basename(file, extname) + '.js');
+        fs.outputFileSync(pathTo.join(entryFile), getEntryFileContent(entryFile, fullpath));
+        entry[name] = pathTo.join(__dirname, entryFile) + '?entry=true';
+      }
+      weexEntry[name] = fullpath + '?entry=true';
+    } else if (stat.isDirectory() && file !== 'build' && file !== 'include') {
+      const subdir = pathTo.join(dir, file);
+      walk(subdir);
+    }
+  });
+}
+// Generate an entry file before writing a webpack configuration
 // walk();
 /**
  * Plugins for webpack configuration.
@@ -183,8 +183,9 @@ const weexConfig = {
   plugins: plugins
 };
 // If The fileType is '.we', only need to use weexConfig for building. 
-if (fileType === '.we') {
-  module.exports = weexConfig;
-} else {
-  module.exports = [webConfig, weexConfig];
-}
+// if (fileType === '.we') {
+//   module.exports = weexConfig;
+// } else {
+//   module.exports = [webConfig, weexConfig];
+// }
+module.exports = [webConfig, weexConfig];
