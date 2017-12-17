8971cbb29fdc376665a062a1c2c470e848adf52c
tenglai <13049322087@163.com>
2017-12-17 11:58:37 +0800

    change webpack.config.js

--- weexapp/webpack.config.js
+++ weexapp/webpack.config.js
@@ -1,189 +1,139 @@
 const pathTo = require('path');
 const fs = require('fs-extra');
 const webpack = require('webpack');
-// 修改入口文件
+
 const entry = {index: pathTo.resolve('src', 'entry.js')};
 const weexEntry = {index: pathTo.resolve('src', 'entry.js')};
 const vueWebTemp = 'temp';
 const hasPluginInstalled = fs.existsSync('./web/plugin.js');
-const isWin = /^win/.test(process.platform);
-let fileType = '';
-// Wraping the entry file
-const getEntryFileContent = (entryPath, vueFilePath) => {
+var isWin = /^win/.test(process.platform);
+
+
+function getEntryFileContent(entryPath, vueFilePath) {
     let relativePath = pathTo.relative(pathTo.join(entryPath, '../'), vueFilePath);
     let contents = '';
-    /**
-     * The plugin's logic currently only supports the .we version
-     * which will be supported later in .vue
-     */
     if (hasPluginInstalled) {
-      const plugindir = pathTo.resolve('./web/plugin.js');
-      contents = 'require(\'' + plugindir + '\') \n';
+        const plugindir = pathTo.resolve('./web/plugin.js');
+        contents = 'require(\'' + plugindir + '\') \n';
     }
     if (isWin) {
-      relativePath = relativePath.replace(/\\/g, '\\\\');
+        relativePath = relativePath.replace(/\\/g,'\\\\');
     }
     contents += 'var App = require(\'' + relativePath + '\')\n';
     contents += 'App.el = \'#root\'\n';
     contents += 'new Vue(App)\n';
     return contents;
-  }
-  // Retrieve entry file mappings by function recursion
-const walk = (dir) => {
+}
+
+var fileType = '';
+
+function walk(dir) {
     dir = dir || '.';
     const directory = pathTo.join(__dirname, 'src', dir);
-    fs.readdirSync(directory).forEach((file) => {
-      const fullpath = pathTo.join(directory, file);
-      const stat = fs.statSync(fullpath);
-      const extname = pathTo.extname(fullpath);
-      // 定义基础路径
-      const basename = pathTo.basename(fullpath);
-      if (stat.isFile() && extname === '.vue' || basename != 'App.vue') {
-        if (!fileType) {
-          fileType = extname;
-        }
-        if (fileType && extname !== fileType) {
-          console.log('Error: This is not a good practice when you use ".we" and ".vue" togither!');
-        }
-        const name = pathTo.join(dir, pathTo.basename(file, extname));
-        if (extname === '.vue') {
-          const entryFile = pathTo.join(vueWebTemp, dir, pathTo.basename(file, extname) + '.js');
-          fs.outputFileSync(pathTo.join(entryFile), getEntryFileContent(entryFile, fullpath));
-          entry[name] = pathTo.join(__dirname, entryFile) + '?entry=true';
-        }
-        weexEntry[name] = fullpath + '?entry=true';
-      } else if (stat.isDirectory() && ['build','include','assets','filters','mixins'].indexOf(file) == -1) {
-        const subdir = pathTo.join(dir, file);
-        walk(subdir);
-      }
-    });
-  }
-  // Generate an entry file before writing a webpack configuration
+    fs.readdirSync(directory)
+        .forEach((file) => {
+            const fullpath = pathTo.join(directory, file);
+            const stat = fs.statSync(fullpath);
+            const extname = pathTo.extname(fullpath);
+            const basename = pathTo.basename(fullpath);
+            if (stat.isFile() && extname === '.vue' && basename != 'App.vue' ) {
+                if (!fileType) {
+                    fileType = extname;
+                }
+                if (fileType && extname !== fileType) {
+                    console.log('Error: This is not a good practice when you use ".we" and ".vue" togither!');
+                }
+                const name = pathTo.join(dir, pathTo.basename(file, extname));
+                if (extname === '.vue') {
+                    const entryFile = pathTo.join(vueWebTemp, dir, pathTo.basename(file, extname) + '.js');
+                    fs.outputFileSync(pathTo.join(entryFile), getEntryFileContent(entryFile, fullpath));
+
+                    entry[name] = pathTo.join(__dirname, entryFile) + '?entry=true';
+                }
+                weexEntry[name] = fullpath + '?entry=true';
+            } else if (stat.isDirectory() && ['build','include','assets','filters','mixins'].indexOf(file) == -1 ) {
+                const subdir = pathTo.join(dir, file);
+                walk(subdir);
+            }
+        });
+}
+
 walk();
-/**
- * Plugins for webpack configuration.
- */
+// web need vue-loader
 const plugins = [
-  /*
-   * Plugin: UglifyJsPlugin
-   * Description: UglifyJS plugin for webpack
-   * See: https://github.com/webpack-contrib/uglifyjs-webpack-plugin
-   */
-  new webpack.optimize.UglifyJsPlugin({
-    minimize: true
-  }),
-  /*
-   * Plugin: BannerPlugin
-   * Description: Adds a banner to the top of each generated chunk.
-   * See: https://webpack.js.org/plugins/banner-plugin/
-   */
-  new webpack.BannerPlugin({
-    banner: '// { "framework": ' + (fileType === '.vue' ? '"Vue"' : '"Weex"') + '} \n',
-    raw: true,
-    exclude: 'Vue'
-  })
+    new webpack.optimize.UglifyJsPlugin({minimize: true}),
+    new webpack.BannerPlugin({
+        banner: '// { "framework": ' + (fileType === '.vue' ? '"Vue"' : '"Weex"') + '} \n',
+        raw: true,
+        exclude: 'Vue'
+    })
 ];
-// Config for compile jsbundle for web.
 const webConfig = {
-  context: pathTo.join(__dirname, ''),
-  entry: entry,
-  output: {
-    path: pathTo.join(__dirname, 'dist'),
-    filename: '[name].web.js'
-  },
-  /**
-   * Developer tool to enhance debugging
-   *
-   * See: http://webpack.github.io/docs/configuration.html#devtool
-   * See: https://github.com/webpack/docs/wiki/build-performance#sourcemaps
-   */
-  // devtool: 'source-map',
-  /*
-   * Options affecting the resolving of modules.
-   *
-   * See: http://webpack.github.io/docs/configuration.html#module
-   */
-  module: {
-    // webpack 2.0 
-    rules: [
-      {
-        test: /\.js$/,
-        use: [{
-          loader: 'babel-loader',
-          options: {
-            presets: ['es2015']
-          }
-        }]
-      },
-      {
-        test: /\.css$/,
-        use: [{
-          loader: 'css-loader'
-        }]
-      },
-      {
-        test: /\.vue(\?[^?]+)?$/,
-        use: [{
-          loader: 'vue-loader'
-        }]
-      }
-    ]
-  },
-  /*
-   * Add additional plugins to the compiler.
-   *
-   * See: http://webpack.github.io/docs/configuration.html#plugins
-   */
-  plugins: plugins
+    context: pathTo.join(__dirname, ''),
+    entry: entry,
+    output: {
+        path: pathTo.join(__dirname, 'dist'),
+        filename: '[name].web.js',
+    },
+    module: {
+        // webpack 2.0
+        rules: [
+            {
+                test: /\.js$/,
+                use: [{
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['es2015']
+                    }
+                }]
+            },
+            {
+                test: /\.css$/,
+                use: [{
+                    loader: 'css-loader'
+                }]
+            },
+            {
+                test: /\.vue(\?[^?]+)?$/,
+                use: [{
+                    loader: 'vue-loader'
+                }]
+            }
+        ]
+    },
+    plugins: plugins
 };
-// Config for compile jsbundle for native.
 const weexConfig = {
-  entry: weexEntry,
-  output: {
-    path: pathTo.join(__dirname, 'dist'),
-    filename: '[name].js'
-  },
-  /*
-   * Options affecting the resolving of modules.
-   *
-   * See: http://webpack.github.io/docs/configuration.html#module
-   */
-  module: {
-    rules: [{
-      test: /\.js$/,
-      use: [{
-        loader: 'babel-loader'
-      }]
-    }, {
-      test: /\.vue(\?[^?]+)?$/,
-      use: [{
-        loader: 'weex-loader'
-      }]
-    }, {
-      test: /\.we(\?[^?]+)?$/,
-      use: [{
-        loader: 'weex-loader'
-      }]
-    }]
-  },
-  /*
-   * These options configure whether to polyfill or mock certain Node.js globals and modules. 
-   * This allows code originally written for the Node.js environment to run in other environments like the browser.
-   *
-   * See: https://webpack.js.org/configuration/node/#node
-   */
-  node: {
-    setImmediate: false
-    // See "Other node core libraries" for additional options.
-  },
-  /*
-   * Add additional plugins to the compiler.
-   *
-   * See: http://webpack.github.io/docs/configuration.html#plugins
-   */
-  plugins: plugins
+    entry: weexEntry,
+    output: {
+        path: pathTo.join(__dirname, 'dist'),
+        filename: '[name].js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: [{
+                    loader: 'babel-loader',
+                }]
+            },
+            {
+                test: /\.vue(\?[^?]+)?$/,
+                use: [{
+                    loader: 'weex-loader'
+                }]
+            },
+            {
+                test: /\.we(\?[^?]+)?$/,
+                use: [{
+                    loader: 'weex-loader'
+                }]
+            }
+        ],
+    },
+    plugins: plugins,
 };
-// If The fileType is '.we', only need to use weexConfig for building. 
+
 var exports = [webConfig, weexConfig];
 
 module.exports = exports;
